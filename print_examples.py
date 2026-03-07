13b5f2200ce8eb9e5af547550c32abc8c8c89c35
anshul6264 <patidaranshul2005@gmail.com>
2026-03-07 16:42:54 +0530

    2nd commit

---
+++ print_examples.py
@@ -0,0 +1,11 @@
+name="Anshul"
+age=21
+city="indore"
+school="Jawahar Navodaya Vidhyalaya"
+print(name, age, city)
+print("My name is",age)
+print("My name is",name,"and I am ",age," years old","and I am from ",city)
+print(f'My name is {name}, and I am {age} years old and I am form {city}')
+print("My name is {},I am {} years old, and I am from {}".format (name,age,city))
+print("My name is"+ name+"and I am from "+ city)
+print(f'Hello everyone, My self {name}, I am from {city}, I completed my schooling from {school}')
